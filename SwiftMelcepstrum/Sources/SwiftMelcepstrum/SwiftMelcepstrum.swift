b0015a0e1e2fc5a765d2f8fef3b839b1cc39a654
BryanWBear <bryan.wang@berkeley.edu>
2020-05-31 19:16:59 -0700

    add spectrogram

--- SwiftMelcepstrum/Sources/SwiftMelcepstrum/SwiftMelcepstrum.swift
+++ SwiftMelcepstrum/Sources/SwiftMelcepstrum/SwiftMelcepstrum.swift
@@ -32,4 +32,51 @@ struct SwiftMelcepstrum {
         }
         return filterBank
     }
+    
+    // every element that isn't index 0 or N/2 needs to be scaled by 1/4, as Accelerate FFT is 2 times regular FFT. 0 and N/2 must be scaled by 1/8 because N/2 is packed into the imaginary component of 0. For more information, consult this lost piece of documentation: https://developer.apple.com/library/archive/documentation/Performance/Conceptual/vDSP_Programming_Guide/UsingFourierTransforms/UsingFourierTransforms.html
+    func getSpectrogram(magnitudes: [Float]) -> [Float] {
+        var scaledMagnitudes = Array(magnitudes[0..<magnitudes.count / 2]).map {x in x * x}
+        let nyquist = scaledMagnitudes.removeFirst() / 8
+        scaledMagnitudes = scaledMagnitudes.map {x in x / 4}
+        scaledMagnitudes.insert(nyquist, at: 0)
+        scaledMagnitudes.append(nyquist)
+        return scaledMagnitudes
+    }
+
+    func spectrogramLibrosa(signal: [Float]) -> [Float] {
+        var window:[Float] = [Float](repeating: 0.0, count: signal.count)
+        vDSP_hann_window(&window, 8, Int32(vDSP_HANN_DENORM))
+        vDSP_vmul(signal, 1, window, 1, &signal, 1, UInt(signal.count))
+        
+        
+        // code from: https://gist.github.com/jeremycochoy/45346cbfe507ee9cb96a08c049dfd34f
+        var length = vDSP_Length(signal.count)
+        var log2n = vDSP_Length(ceil(log2(Float(length * 2))))
+        var fftSetup = vDSP.FFT(log2n: log2n, radix: .radix2, ofType: DSPSplitComplex.self)!
+
+        // input and output arrays
+        var forwardInputReal = [Float](signal)
+        var forwardInputImag = [Float](repeating: 0, count: Int(length))
+        var forwardOutputReal = [Float](repeating: 0, count: Int(length))
+        var forwardOutputImag = [Float](repeating: 0, count: Int(length))
+        var magnitudes = [Float](repeating: 0, count: Int(length))
+
+        // compute FFT
+        forwardInputReal.withUnsafeMutableBufferPointer { forwardInputRealPtr in
+          forwardInputImag.withUnsafeMutableBufferPointer { forwardInputImagPtr in
+            forwardOutputReal.withUnsafeMutableBufferPointer { forwardOutputRealPtr in
+              forwardOutputImag.withUnsafeMutableBufferPointer { forwardOutputImagPtr in
+                
+                let forwardInput = DSPSplitComplex(realp: forwardInputRealPtr.baseAddress!, imagp: forwardInputImagPtr.baseAddress!)
+                var forwardOutput = DSPSplitComplex(realp: forwardOutputRealPtr.baseAddress!, imagp: forwardOutputImagPtr.baseAddress!)
+
+                fftSetup.forward(input: forwardInput, output: &forwardOutput)
+                vDSP.absolute(forwardOutput, result: &magnitudes)
+              }
+            }
+          }
+        }
+        
+        return getSpectrogram(magnitudes: magnitudes)
+    }
 }
